3bf87b12cad6364b1998ce1b8a06e039ae4961d3
Srinivas <snthelion@gmail.com>
2020-04-16 21:01:01 -0500

    Style changes for pager section

--- assets/local/js/datatable_util.js
+++ assets/local/js/datatable_util.js
@@ -343,8 +343,8 @@ function setDatatableDOM(dtoptions, dtSettings) {
                 ">r<'row dt-helpbox'<'col-md-12 col-xs-12 dt-help-content'>>t";
             //if (scroller === '') 
             {
-                dtSettings["dom"] = dtdom1 + "<'row dt-pager-row'<'col-md-3 col-xs-12 hidden-xs1 dt-pager-countinfo'i>" +
-                    "<'col-md-9 col-xs-12 dt-pager-pages text-right tablesorter-pager'p>" +
+                dtSettings["dom"] = dtdom1 + "<'dt-pager-row'" +
+                    "<'dt-pager-pages text-right tablesorter-pager'p><'dt-pager-countinfo'i>" +
                     ">>";
             }
             /* else {
