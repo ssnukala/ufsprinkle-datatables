bea3ef453272fbeddc2767002415605b965217ea
ssnukala <snthelion@gmail.com>
2022-01-09 13:51:17 -0600

    Update DatatablesController.php
    Added randm string function

--- src/Controller/DatatablesController.php
+++ src/Controller/DatatablesController.php
@@ -491,4 +491,15 @@ class DatatablesController extends SimpleController
         $result['pagination'] = ['more' => ($result['recordsFiltered'] > 10)];
         return $response->withJson($result, 200, JSON_PRETTY_PRINT);
     }
+
+    public static  function randomString($length = 10)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 }
